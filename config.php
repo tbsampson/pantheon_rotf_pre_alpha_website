887b3075e88d83e5669d8ede3e5d213aaa9f2683
tbsampson <tbsampson@gmail.com>
2014-02-02 10:23:32 -0600

    Fixed Typos
    Typos and added alt/title to images

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
 $image_path="images/";
 
 // Header
-$base_ref="http://www.pantheonrotf.com";
+$base_ref="http://www.woodlands.pro";
 $header_description="Pantheon: Rise of the Fallen MMORPG.  Join the fast growing community for the upcoming title by creator of EverQuest and Vanguard: Saga of Heroes creator Brad McQuaid.";
 $header_keywords="Pantheon, Rise of the Fallen, RotF, MMORPG, Unity3D, EverQuest, Brad McQuaid, Vanguard, Saga of Heroes.";
 $kickstarter_rotf_home="https://www.kickstarter.com/projects/1588672538/pantheon-rise-of-the-fallen/";
@@ -28,7 +28,7 @@ $revenant_image="Revenant_150.jpg";
   $banner_text="Pantheon: Rise of the Fallen is a group-focused MMORPG based on challenging gameplay and open world high fantasy. It takes place on a world populated by deities and heroes, called Terminus. Mortal and planar shards which have collided create the dramatic and epic environment of Terminus. With some of those shards came deities and demigods that, over time, raised empires which forged the turbulent political landscape. Centuries later, new planar collisions have occurred that will pull the players into the world and its politics. The player is a legendary hero, stripped of his or her powerful relics and left to explore the dramatically diverse and epic regions of Terminus with hopes of reclaiming their relics and those of lost heroes.";
 $crusader_text="The Crusader is a powerful knight whose devotion to king or deity is so great that he has been blessed with divine powers. A paragon of...";
   $cleric_text="The Cleric is battle-ready priest devoted to spreading the influence of his or her pantheon while adventuring within the wilds of Terminus. Armed...";
-$revenant_text="Faithless in life and bound by ancient rites, Revenants have been doomed to walk the lands of Terminus as the undead. Once nothing more than...";
+$revenant_text="Faithless in life and bound by ancient rites, Revenants have been doomed to walk the lands of Terminus as the undead. Once nothing more...";
 $more_text="READ MORE!";
 
 //Footer
